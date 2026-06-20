ec9313d04d7bcc2b4e68312ff77922d4902b2117
rdk31 <rdk31@protonmail.com>
2026-06-20 12:04:56 +0200

    use nix-github-actions

--- flake.nix
+++ flake.nix
@@ -14,34 +14,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ self, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
       ];
-      systems = [
-        "x86_64-linux"
-      ];
+      systems = [ "x86_64-linux" ];
+      flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix { checks = self.packages; };
       perSystem =
         { config, pkgs, ... }:
-        let
-          TEM-simulator = pkgs.callPackage ./pkgs/TEM-simulator.nix { };
-          relion = pkgs.callPackage ./pkgs/relion.nix { };
-          ctffind = pkgs.callPackage ./pkgs/ctffind.nix { };
-          cisTEM = pkgs.callPackage ./pkgs/cisTEM.nix { };
-        in
         {
           packages = {
-            inherit
-              TEM-simulator
-              relion
-              ctffind
-              cisTEM
-              ;
+            TEM-simulator = pkgs.callPackage ./pkgs/TEM-simulator.nix { };
+            relion = pkgs.callPackage ./pkgs/relion.nix { };
+            ctffind = pkgs.callPackage ./pkgs/ctffind.nix { };
+            cisTEM = pkgs.callPackage ./pkgs/cisTEM.nix { };
           };
           overlayAttrs = {
             inherit (config.packages)
